f6560683a14047f4d2f815a168772f7d88a9771f
rueeeeeeng <qhaud7023@naver.com>
2022-09-03 20:23:56 +0900

    백준 문자열-아스키코드 소스코드

---
+++ BaekJoon/src/no6/AsciiCode.java
@@ -0,0 +1,17 @@
+package no6;
+
+import java.util.Scanner;
+
+public class AsciiCode {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char n = sc.next().charAt(0);
+		
+		System.out.println((int)n);
+		
+		
+	}
+
+}
